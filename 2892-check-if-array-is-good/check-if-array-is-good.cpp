d7c133b7c68785a731be8674545408140df29442
notsparkieee <136390046+notsparkieee@users.noreply.github.com>
2026-05-17 11:26:21 +0530

    Time: 0 ms (100.00%) | Memory: 26.4 MB (67.36%) - LeetSync

--- 2892-check-if-array-is-good/check-if-array-is-good.cpp
+++ 2892-check-if-array-is-good/check-if-array-is-good.cpp
@@ -3,14 +3,12 @@ public:
     bool isGood(vector<int>& nums) {
         int n = nums.size();
         int maxi = *max_element(nums.begin(),nums.end());
-        if(nums.size()!=maxi+1) return false;
-         unordered_map<int,int>mpp;
-         for(auto it : nums) mpp[it]++;
-         for(auto it : mpp){
-            if(it.first!=maxi && it.second!=1) return false;
-            if(it.first==maxi && it.second!=2) return false;
-         }
-         return true;
- 
+        if(n!=maxi+1) return false;
+        sort(nums.begin(),nums.end());
+       for(int i=0;i<n-1;i++){
+        if(nums[i]!=i+1) return false;
+       } 
+       if(nums[n-1]==maxi && nums[n-2]==maxi) return true;
+       return false;
     }
 };
