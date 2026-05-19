7a48d7f341fdd5b46189074e5d9c9ef01f8bbb1a
SIDDHANT KHARE <136390046+notsparkieee@users.noreply.github.com>
2026-05-19 19:01:05 +0530

    Time: 0 ms (100.00%) | Memory: 20.4 MB (60.00%) - LeetSync

--- 45-jump-game-ii/jump-game-ii.cpp
+++ 45-jump-game-ii/jump-game-ii.cpp
@@ -1,22 +1,17 @@
 class Solution {
-    int dp[11006];
-    int rec(int i,vector<int>&nums){
-        int n = nums.size();
-        if(dp[i]!=-1) return dp[i];
-        if(i==n-1) return 0;
-        if(i>n-1) return 1e9;
-        int mini = 1e9;
-        for(int j=1;j<=nums[i];j++){
-            mini = min(mini,1+rec(i+j,nums));
-        }
-        return dp[i]=mini;
-
-    }
 public:
     int jump(vector<int>& nums) {
         int n = nums.size();
-        memset(dp,-1,sizeof(dp));
-        return rec(0,nums);
-
+        int currentEnd = 0;
+        int farthest = 0;
+        int jumps = 0;
+        for(int i=0;i<n-1;i++){
+            farthest = max(farthest,nums[i]+i);
+            if(i==currentEnd){
+                jumps++;
+                currentEnd = farthest;
+            } 
+        }
+        return jumps;
     }
 };
