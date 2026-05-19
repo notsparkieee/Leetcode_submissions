2be8ffb38d7de89adfe976c6206275e790e866bb
SIDDHANT KHARE <136390046+notsparkieee@users.noreply.github.com>
2026-05-19 19:15:56 +0530

    Time: 0 ms (100.00%) | Memory: 52.2 MB (81.63%) - LeetSync

--- 55-jump-game/jump-game.cpp
+++ 55-jump-game/jump-game.cpp
@@ -1,32 +1,13 @@
 class Solution {
-    int dp[10005];
-    bool rec(int i,vector<int>&nums){
-        int n = nums.size();
-        if(i>=n){
-            return 0;
-        }
-        if(i==n-1){
-            return 1;
-        }
-        if(dp[i]!=-1) return dp[i];
-        int count = 0;
-        for(int j=1;j<=nums[i];j++){
-            if(rec(i+j,nums)){
-                return dp[i] = 1;
-            }
-        }
-        return dp[i]=false;
-        
-
-    }
 public:
     bool canJump(vector<int>& nums) {
         int n = nums.size();
-                memset(dp,-1,sizeof(dp));
-
-        bool cnt = rec(0,nums);
-        return cnt;
-
-        
+        int farthest = 0;
+        for(int i=0;i<n;i++){
+            if(farthest<i) return 0;
+            farthest = max(farthest,i+nums[i]);
+        }
+        if(farthest>=n-1) return 1;
+        return 0;
     }
 };
