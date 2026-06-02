2fae1188c84da34881ad792e6f9a927496860978
SIDDHANT KHARE <136390046+notsparkieee@users.noreply.github.com>
2026-06-02 19:28:45 +0530

    Time: 0 ms (100.00%) | Memory: 11.2 MB (81.87%) - LeetSync

--- 1878-check-if-array-is-sorted-and-rotated/check-if-array-is-sorted-and-rotated.cpp
+++ 1878-check-if-array-is-sorted-and-rotated/check-if-array-is-sorted-and-rotated.cpp
@@ -4,7 +4,7 @@ public:
         int n = nums.size();
         int cnt = 0;
         for(int i=0;i<n;i++){
-            if(nums[i]>nums[(i+1)%n])cnt++;
+            if(nums[i]>nums[(i+1)%n]) cnt++;
         }
         return cnt<=1;
     }
